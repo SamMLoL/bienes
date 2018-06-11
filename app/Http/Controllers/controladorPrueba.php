123044eb7269238aba65562f647e711b2b1174ea
SamMLoL <samm.arteaga@gmail.com>
2018-06-11 17:13:02 -0400

    primer push

---
+++ app/Http/Controllers/controladorPrueba.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class controladorPrueba extends Controller
+{
+    public function index()
+    {
+    	echo "hola mundo";
+    }
+}
